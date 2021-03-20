9546d25f173ee96f3663dd911304b7c4a42d8cdf
ron1214x <74463188+ron1214x@users.noreply.github.com>
2021-03-20 13:00:11 +0900

    M1 Apple Silicon dotfiles update

--- nvim/nvim/plug.vim 설치.sh
+++ nvim/nvim/plug.vim 설치.sh
@@ -1,2 +1,2 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
